c9b687275e64c0559dd56be816099b0107ed6982
aniconsultant <43361858+aniforverizon@users.noreply.github.com>
2021-04-07 20:11:19 +0530

    Create containerscript.sh

---
+++ containerscript.sh
@@ -0,0 +1,6 @@
+achaturv1@idp01 ~ $ cat containerscript.sh
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -w fs.file-max=65536
+ulimit -n 65536
+ulimit -u 4096
+docker run -d --restart unless-stopped 46a 4ec
